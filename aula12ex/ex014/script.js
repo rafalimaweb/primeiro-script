9b58636c81305708d7277a57c37297ad11c690f8
Rafael da Silva Lima <79911403+rafalimaweb@users.noreply.github.com>
2023-05-15 08:10:25 -0300

    Hora do dia

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('tam')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${min} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'manha.png'
+        document.body.style.background = '#ffcdab'
+    }else if (hora >= 12 && hora <=18){
+        //BOA TARDE
+        img.src = 'tarde.png'
+        document.body.style.background = '#d19c58'
+    }else {
+        //BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background = '#342640'
+    }
+
+}
+
