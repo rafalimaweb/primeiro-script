92b7bc9e0778c4ea93cd58fa08a8e50415fd179c
Rafael da Silva Lima <79911403+rafalimaweb@users.noreply.github.com>
2023-05-17 08:36:01 -0300

    contador

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,32 @@
-alert('Olá')
+function contagem(){
+    var ini = document.getElementById('num1')
+    var fim = document.getElementById('num2')
+    var pass = document.getElementById('cont')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 ||pass.value.length == 0 ){
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    }else{
+       res.innerHTML = 'Contando: <br> '
+       var i = Number(ini.value)
+       var f = Number(fim.value)
+       var pas = Number(pass.value)
+       if (pas<=0){
+        window.alert('Passo inválido! Considerando PASSO 1')
+        pas = 1
+       } 
+       if (i < f){
+            // Contagem crescente
+            for(var c = i; c <= f; c += pas ){
+                res.innerHTML +=  `${c} \u{1F449} `
+            }
+       }else{
+            // Contagem regressiva
+            for(var c = i; c >= f; c -= pas){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+       }
+       res.innerHTML  += `\u{1F3C1}`
+    }
+}
